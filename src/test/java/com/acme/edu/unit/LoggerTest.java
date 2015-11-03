65eb6cdc97cf281515ec950c358f8e230408c74c
vorobey92 <vorobey92@yandex.ru>
2015-11-03 16:56:34 +0300

    Iteration 08: 3 unit tests with State (log(int))

--- src/test/java/com/acme/edu/unit/LoggerTest.java
+++ src/test/java/com/acme/edu/unit/LoggerTest.java
@@ -13,17 +13,45 @@ import static org.mockito.Mockito.*;
 
 public class LoggerTest {
 
+    private Printable mock;
+    private State sut;
 
 
     @Test
-    public void shouldPrint(){
-        Printable mock = mock(ConsolePrinter.class);
-        State sut = new IntState(mock);
+    public void shouldPrintOnePrimitiveWhenYouTryingToLogOneNumber(){
+        mock = mock(ConsolePrinter.class);
+        sut = new IntState(mock);
 
         sut.log("2");
         sut.flush();
 
         verify(mock).println("primitive: 2");
+    }
+
+    @Test
+    public void shouldLogSumOfPrimitivesWhenYouTryingToLogSequenceOfInts(){
+        mock = mock(ConsolePrinter.class);
+        sut = new IntState(mock);
+
+        sut.log("2");
+        sut.log("5");
+        sut.log("3");
+        sut.flush();
+
+        verify(mock).println("primitive: 10");
+    }
+
+    @Test
+    public void shouldLogTwoPrimitivesWhenAppearsTypeOverflowWhileSumming(){
+        mock = mock(ConsolePrinter.class);
+        sut = new IntState(mock);
+
+        sut.log("1");
+        sut.log("2");
+        sut.log("" + (Integer.MAX_VALUE-1));
+
+        verify(mock).println("primitive: 3");
+        verify(mock).println("primitive: " + (Integer.MAX_VALUE-1));
     }
 
 }
