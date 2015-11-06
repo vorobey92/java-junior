4abd7c9e0bc58cf047a268b4d07e996aee231230
elfray <ulaelfray@gmail.com>
2015-11-06 14:37:41 +0300

    Iteration 13: PrinterException class was updated

--- src/main/java/com/acme/edu/printers/PrinterException.java
+++ src/main/java/com/acme/edu/printers/PrinterException.java
@@ -1,4 +1,9 @@
 package com.acme.edu.printers;
 
+import java.io.IOException;
+
 public class PrinterException extends Exception {
+    public PrinterException(Exception e) {
+        initCause(e);
+    }
 }
