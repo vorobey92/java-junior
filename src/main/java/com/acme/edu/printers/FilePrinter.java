b29d24e4d9ff97581326a1b484b211fb3e56d0bb
elfray <ulaelfray@gmail.com>
2015-11-06 14:40:18 +0300

    Iteration 13: FilePrinter was implemented

---
+++ src/main/java/com/acme/edu/printers/FilePrinter.java
@@ -0,0 +1,46 @@
+package com.acme.edu.printers;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class FilePrinter implements Printer {
+    private File file;
+    private String charSet;
+
+    public FilePrinter(File file, String charSet) {
+        this.file = file;
+        this.charSet = charSet;
+    }
+
+    @Override
+    public void print(String stringToPrint) throws PrinterException {
+        try(PrintWriter printWriter =
+                    new PrintWriter(
+                            new BufferedWriter(
+                                    new OutputStreamWriter(
+                                            new FileOutputStream(file), charSet)))) {
+            printWriter.print(stringToPrint);
+        } catch (IOException e) {
+            throw new PrinterException(e);
+        }
+
+    }
+
+    @Override
+    public void println(String stringToPrint) throws PrinterException {
+        try(PrintWriter printWriter =
+                    new PrintWriter(
+                            new BufferedWriter(
+                                    new OutputStreamWriter(
+                                            new FileOutputStream(file), charSet)))) {
+            printWriter.println(stringToPrint);
+        } catch (IOException e) {
+            throw new PrinterException(e);
+        }
+
+    }
+}
