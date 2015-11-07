e7548efc80a147dd3c07978c7ae646a1b79ded47
elfray <ulaelfray@gmail.com>
2015-11-07 23:02:15 +0300

    Iteration 13: abstraction BufferedLogWriter was added

---
+++ src/main/java/com/acme/edu/printers/BufferedLogWriter.java
@@ -0,0 +1,26 @@
+package com.acme.edu.printers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BufferedLogWriter implements LogWriter {
+    private static final int BUFFER_SIZE = 50;
+    private List<String> buffer = new ArrayList<>(BUFFER_SIZE);
+
+    @Override
+    public void println(String stringToPrint) throws LogWriterException {
+        buffer.add(stringToPrint);
+
+        if (buffer.size() < BUFFER_SIZE) {
+            return;
+        }
+
+        try {
+            write(buffer);
+        } finally {
+            buffer.clear();
+        }
+    }
+
+    protected abstract void write(List<String> buffer) throws LogWriterException;
+}
