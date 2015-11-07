e02b40b5eeeed3a54e41f588ef588b48c15d8756
elfray <ulaelfray@gmail.com>
2015-11-07 11:20:46 +0300

    Iteration 13: New constructor was added to PrinterException class

--- src/main/java/com/acme/edu/printers/PrinterException.java
+++ src/main/java/com/acme/edu/printers/PrinterException.java
@@ -1,6 +1,11 @@
 package com.acme.edu.printers;
 
 public class PrinterException extends Exception {
+
+    public PrinterException(String message) {
+        super(message);
+    }
+
     public PrinterException(String message, Exception e) {
         super(message);
         initCause(e);
