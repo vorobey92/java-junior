bd8ded81d124c24bac43e750ffd4a003e4c7500c
elfray <ulaelfray@gmail.com>
2015-11-09 22:21:11 +0300

    Iteration 15: RemoteBufferWriterLogServerTest was renamed and updated to use RemoteBufferWriter class

--- src/test/java/com/acme/edu/integration/RemoteBufferWriterLogServerTest.java
+++ src/test/java/com/acme/edu/integration/RemoteBufferWriterLogServerTest.java
@@ -1,9 +1,9 @@
 package com.acme.edu.integration;
 
 import com.acme.edu.businessexceptions.LoggerException;
-import com.acme.edu.clientserver.Client;
 import com.acme.edu.clientserver.LogServer;
 import com.acme.edu.printers.LogWriterException;
+import com.acme.edu.printers.RemoteBufferWriter;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
@@ -19,8 +19,7 @@ import java.util.Random;
 
 import static org.fest.assertions.Assertions.*;
 
-
-public class ClientServerTest {
+public class RemoteBufferWriterLogServerTest {
     private int port = 5000 + new Random(System.currentTimeMillis()).nextInt(60000);
     private String localhost = "127.0.0.1";
     private List<String> stringsToBeLogged = Arrays.asList("test string");
@@ -37,16 +36,16 @@ public class ClientServerTest {
     }
 
     @Test
-    public void clientInMainThreadSmokeTest() throws LoggerException, InterruptedException, LogWriterException, IOException {
-        LogServer server = new LogServer(port, getFileName());
-        Client client = new Client(localhost, port);
+    public void remoteBufferWriterInMainThreadSmokeTest() throws LoggerException, InterruptedException, LogWriterException, IOException {
+        LogServer server = new LogServer(port, getFileName(), charset);
+        RemoteBufferWriter client = new RemoteBufferWriter(localhost, port);
 
         Thread serverThread = new Thread(server);
         serverThread.setDaemon(true);
         serverThread.start();
         Thread.sleep(1000);
 
-        client.sendData(stringsToBeLogged, charset);
+        client.writeBuffer(stringsToBeLogged);
 
         assertThat(FileUtils.readLines(new File(getFileName()), charset)).isEqualTo(stringsToBeLogged);
     }
