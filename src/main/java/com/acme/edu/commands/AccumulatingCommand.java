8e6bd07c8f6f4f72941e712efde011e171eba03c
elfray <ulaelfray@gmail.com>
2015-11-09 08:40:46 +0300

    Iteration 13: javadocs for AccumulatingCommand class were added

--- src/main/java/com/acme/edu/commands/AccumulatingCommand.java
+++ src/main/java/com/acme/edu/commands/AccumulatingCommand.java
@@ -3,12 +3,29 @@ package com.acme.edu.commands;
 import com.acme.edu.businessexceptions.LoggingException;
 import com.acme.edu.printers.LogWriter;
 
+/**
+ * An AccumulatingCommand is subclass of Command and provides a skeletal implementation for command which accumulate
+ * data.
+ */
 public abstract class AccumulatingCommand extends Command {
 
+    /**
+     * Constructs a new AccumulatingCommand object with provided array of LogWriters
+     * @param logWriters an array of LogWriters which will be used for writing a message exception
+     */
     public AccumulatingCommand(LogWriter... logWriters) {
         super(logWriters);
     }
 
+    /**
+     * Merges this command with another command. If another command is null or does not have a message then just
+     * this command is returned. If another command is of different class, then another command is executed and
+     * this command is returned. Exact behaviour of merging with another command of the same class is definded
+     * by subclasses.
+     * @param oldCommand a Command to be merged with this command
+     * @return the resulting Command object or this Command if accumulating is not applicable between commands
+     * @throws LoggingException if errors occur when logging the message
+     */
     @Override
     public Command merge(Command oldCommand) throws LoggingException {
         if (oldCommand == null || oldCommand.getMessage() == null) {
