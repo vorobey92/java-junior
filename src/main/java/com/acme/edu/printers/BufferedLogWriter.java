2a711fc0794bac2475e2cbabd79de7f009149c95
elfray <ulaelfray@gmail.com>
2015-11-09 20:59:16 +0300

    Iteration 15: message prioritization mechanism was added to BufferedLogWriter class

--- src/main/java/com/acme/edu/printers/BufferedLogWriter.java
+++ src/main/java/com/acme/edu/printers/BufferedLogWriter.java
@@ -1,10 +1,17 @@
 package com.acme.edu.printers;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public abstract class BufferedLogWriter implements LogWriter {
     private static final int BUFFER_SIZE = 50;
+    private static final String HIGH_PRIORITY_SUBSTRING = "ERROR";
+    private static final Comparator<String> MESSAGE_COMPARATOR =
+            (message1, message2)
+                    -> Boolean.compare(containsHighPrioritySubstring(message1), containsHighPrioritySubstring(message2));
+
     private List<String> buffer = new ArrayList<>(BUFFER_SIZE);
 
     @Override
@@ -15,6 +22,8 @@ public abstract class BufferedLogWriter implements LogWriter {
             return;
         }
 
+        Collections.sort(buffer, MESSAGE_COMPARATOR);
+
         try {
             writeBuffer(buffer);
         } finally {
@@ -23,4 +32,8 @@ public abstract class BufferedLogWriter implements LogWriter {
     }
 
     protected abstract void writeBuffer(List<String> buffer) throws LogWriterException;
+
+    private static boolean containsHighPrioritySubstring(String message) {
+        return message.contains(HIGH_PRIORITY_SUBSTRING);
+    }
 }
