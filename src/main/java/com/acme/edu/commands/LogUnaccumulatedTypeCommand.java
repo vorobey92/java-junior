07019aa4bcfb9222f8c142290efd600766866ab1
elfray <ulaelfray@gmail.com>
2015-11-09 14:38:27 +0300

    Iteration 13: javadocs for LogUnaccumulatingTypeCommand class were added

--- src/main/java/com/acme/edu/commands/LogUnaccumulatedTypeCommand.java
+++ src/main/java/com/acme/edu/commands/LogUnaccumulatedTypeCommand.java
@@ -4,14 +4,29 @@ import com.acme.edu.businessexceptions.LoggingException;
 import com.acme.edu.decorators.Decorator;
 import com.acme.edu.printers.LogWriter;
 
+/**
+ * LogUnaccumulatedTypeCommand is subclass of Command and implements unaccumulating behaviour of
+ * interaction between commands
+ */
 public class LogUnaccumulatedTypeCommand extends Command {
     private Decorator decorator;
 
+    /**
+     * Constructs a new LogUnaccumulatedTypeCommand object with provided array of LogWriters
+     * @param logWriters an array of LogWriters which will be used for writing a message exception
+     */
     public LogUnaccumulatedTypeCommand(Decorator decorator, LogWriter... logWriters) {
         super(logWriters);
         this.decorator = decorator;
     }
 
+    /**
+     * Merges this command with another command. This is done by executing the argument if it is not null and has
+     * non-null message and executing this command in precisely the order given.
+     * @param oldCommand a Command to be merged with this command
+     * @return the resulting Command object or this Command if accumulating is not applicable between commands
+     * @throws LoggingException if errors occur when logging the message
+     */
     @Override
     public Command merge(Command oldCommand) throws LoggingException {
         if (oldCommand != null && oldCommand.getMessage() != null) {
