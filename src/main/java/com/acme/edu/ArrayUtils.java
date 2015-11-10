340507f0798b285a1f5dab663f10de89cd5c4f00
elfray <ulaelfray@gmail.com>
2015-11-10 14:43:12 +0300

    Iteration 15: javadocs for ArrayUtils class were added

--- src/main/java/com/acme/edu/ArrayUtils.java
+++ src/main/java/com/acme/edu/ArrayUtils.java
@@ -4,12 +4,22 @@ import com.acme.edu.businessexceptions.IllegalArgumentException;
 
 import java.util.Arrays;
 
+/**
+ * This class contains methods for manipulating arrays
+ */
 public class ArrayUtils {
 
     private ArrayUtils() {
 
     }
 
+    /**
+     * Reduces the passed multidimensional int array to one dimensional int array containing the elements
+     * of the source array
+     * @param multiDimArray a multidimensional int array
+     * @return a one-dimensional int array containing the elements of the list
+     * @throws IllegalArgumentException if the specified array is null or it contains null
+     */
     public static int[] multiDimIntArrayToOneDimIntArray(Object[] multiDimArray) throws IllegalArgumentException {
         if (multiDimArray == null) {
             throw new IllegalArgumentException("The passed multidimensional array is null or has null element");
