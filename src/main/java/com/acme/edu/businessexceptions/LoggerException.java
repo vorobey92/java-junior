7759dcbb05602fe00832865b01bd31f92e907043
elfray <ulaelfray@gmail.com>
2015-11-10 15:05:44 +0300

    Iteration 15: javadocs for LoggerClass class were added

--- src/main/java/com/acme/edu/businessexceptions/LoggerException.java
+++ src/main/java/com/acme/edu/businessexceptions/LoggerException.java
@@ -2,19 +2,35 @@ package com.acme.edu.businessexceptions;
 
 import java.util.List;
 
+/**
+ * Signals that exceptional situations of some sort has occurred while using Logger
+ */
 public class LoggerException extends Exception {
 
     private List<Exception> causes;
 
+    /**
+     * Constructs a new exception with the specified detail message.
+     * @param message the detail message
+     */
     public LoggerException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new exception with the specified detail message and the specified list of causes -
+     * exceptions that caused this exception to get thrown
+     * @param message the detail message
+     * @param causes the list of causes (which is saved for later retrieval by the getCauses() method
+     */
     public LoggerException(String message, List<Exception> causes) {
         super(message);
         this.causes = causes;
     }
 
+    /**
+     * @return the array of causes of this exception
+     */
     public Exception[] getCauses() {
         if (causes == null) {
             return new Exception[0];
